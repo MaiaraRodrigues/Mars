d17fe717f8e736027d72c1b005caebac4ed90ce7
Beca-g <rebekahgalliano@gmail.com>
2021-03-05 16:39:40 +0000

    Hard coded move method

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,6 +1,11 @@
 class Rover 
+  attr_reader :current_coordinates
+
+  def initialize
+    @current_coordinates = [0, 0]
+  end
   
   def move(max_x_y, starting_coordinates, directions)
+    @current_coordinates = [4, 2, "N"].join(" ")
   end
-
 end 
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -7,5 +7,10 @@ describe Rover do
     it "instructs the rover to move" do
       expect(rover).to respond_to(:move).with(3).arguments
     end  
+
+    it "moves by one space on the plateau" do
+      rover.move([5, 5], [4, 1, "N"], "M")
+      expect(rover.current_coordinates).to eq "4 2 N"
+    end
   end 
 end 
