3852f0ecaf2f2fa20d04ea73954fb9564dbc6c54
Maiara <maiarinhah.eu@gmail.com>
2021-03-09 13:08:48 +0000

    Rover now moves two squares and turns left

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,5 +1,8 @@
 class Rover 
   attr_reader :current_coordinates
+  LEFT_MOVES = { "N" => "W", "W" => "S","S" => "E","E" => "N" }
+  RIGHT_MOVES = {"N" => "E", "E" => "S", "S" => "W", "W" => "N"}
+  
 
   def initialize
     @current_coordinates = current_coordinates
@@ -8,10 +11,29 @@ class Rover
   def move(max_x_y, starting_coordinates, directions)
     directions.chars.each do | letter |
       if letter == "M" 
-        starting_coordinates[1] +=1
+        if starting_coordinates[2] == "N"
+          starting_coordinates[1] +=1
+        elsif starting_coordinates[2] == "S"
+          starting_coordinates[1] -=1
+        elsif starting_coordinates[2] == "E"
+          starting_coordinates[0] +=1
+        else starting_coordinates[2] == "W"
+          starting_coordinates[0] -=1
+        end
+      else letter == "L"
+        starting_coordinates[2] = LEFT_MOVES[starting_coordinates[2]]
       end 
     end 
       @current_coordinates = starting_coordinates
   end
 end 
+#  4 is x 
+#  1 is y 
+#  n is the direction 
+
+# if the heading is north is +1 
+# north +1 on y 
+# south -1 on y 
+# east +1 on x
+# west -1 on x 
 
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -12,5 +12,12 @@ describe Rover do
       rover.move([5, 5], [4, 1, "N"], "M")
       expect(rover.current_coordinates).to eq [4, 2, "N"]
     end
+
+    it "moves two squares and turn left" do
+      rover.move([5, 5], [4, 1, "N"], "MLM")
+      expect(rover.current_coordinates).to eq [3, 2, "W"]
+    end
+
+
   end 
 end 
