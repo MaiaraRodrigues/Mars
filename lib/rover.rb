dd8e9d0706c36ff541b8445e1a2b0d740cdae828
Maiara <maiarinhah.eu@gmail.com>
2021-03-09 13:36:41 +0000

    Extrated left moves into private method

--- lib/rover.rb
+++ lib/rover.rb
@@ -13,7 +13,7 @@ class Rover
       if letter == "M" 
         m_direction(starting_coordinates)
       elsif letter == "L"
-        starting_coordinates[2] = LEFT_MOVES[starting_coordinates[2]]
+        left_directions(starting_coordinates)
       else letter == "R"
         starting_coordinates[2] = RIGHT_MOVES[starting_coordinates[2]]
       end 
@@ -34,6 +34,10 @@ class Rover
       starting_coordinates[0] -= 1
     end
   end  
+
+  def left_directions(starting_coordinates)
+    starting_coordinates[2] = LEFT_MOVES[starting_coordinates[2]]
+  end 
 end 
 #  4 is x 
 #  1 is y 
